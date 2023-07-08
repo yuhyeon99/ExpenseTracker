d1e87c810c2a315ab4d8b51b98f1785a3bc05860
yuhyeon99 <jujini31@gmail.com>
2023-07-08 22:11:54 +0900

    feat : 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, {useState, useEffect} from "react";
 import { atom } from 'recoil';
 
 import './App.css';
-import TrackerStatus from './components/TrackerContainer';
+import TrackerContainer from './components/TrackerContainer';
 import ItemAtom from './recoil/ItemAtoms';
 
 function App() {
 
-  const Item = useRecoilValue(ItemAtom);
+  const item = useRecoilValue(ItemAtom);
 
   useEffect(()=>{
 
   }, []);
 
   return (
-    <div>
-      
-    </div>
+    <TrackerContainer items={item} />
   );
 }
 
--- src/recoil/ItemAtom.js
+++ src/recoil/ItemAtom.js
@@ -4,7 +4,7 @@ export const ItemAtom = atom({
     key : 'ItemAtom',
     default : JSON.parse(localStorage.getItem('ItemAtom')) || []
 });
-
+// 한 객체당 (날짜, 제목, 수입|지출여부, 금액)
 ItemAtom.subscribe((newItems)=>{
     localStorage.setItem('ItemAtom', JSON.stringify(newItems));
 });
