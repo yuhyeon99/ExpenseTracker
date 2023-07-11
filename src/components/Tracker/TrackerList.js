a9c7367e105aac03566cbf6e9f790358d412daf4
yuhyeon99 <jujini31@gmail.com>
2023-07-11 17:24:17 +0900

    feat : 전체 UI 퍼블리싱

--- src/components/Tracker/TrackerList.js
+++ src/components/Tracker/TrackerList.js
@@ -1,10 +1,22 @@
 import React,{useState, useEffect} from 'react';
 
+import './TrackerList.css';
+
 const TrackerList = () => {
 
     return(
-        <div>
-
+        <div className="tracker-list">
+            <div className="list-type">
+                <p className="active">전체 내역</p>
+                <p>수입</p>
+                <p>지출</p>
+            </div>
+            <div className="list-items">
+                <div>
+                    <img src="/img/new_list.png" alt="new_list" />
+                    <p>새 거래 내역을 추가하세요</p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Tracker/TrackerStatus.js
+++ src/components/Tracker/TrackerStatus.js
@@ -8,8 +8,32 @@ const TrackerStatus = () => {
         <div className="tracker_status">
             <div className="status_header">
                 <p>작심소비</p>
-                <hr />
             </div>
+
+            <div className="status_about">
+                <img src="/img/logo.png"/>
+                <p>안녕하세요!</p>
+                <p>지난 1개월 간의 거래 내역을</p>
+                <p>확인해보세요.</p>
+                <p className="money">&#8361;0</p>
+
+                <div className="about_box">
+                    <ul>
+                        <li className="box_in">
+                            <p>들어온 돈</p>
+                            <p className="box_money">0</p>
+                        </li>
+                        <li className="box_border"></li>
+                        <li className="box_out">
+                            <p>나간 돈</p>
+                            <p className="box_money">-0</p>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+
+            <div className="border-block"></div>
         </div>
     );
 }
