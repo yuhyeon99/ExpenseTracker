db7c783290bd772ef1c8286f0d84234cd951b13a
yuhyeon99 <jujini31@gmail.com>
2023-07-14 17:41:32 +0900

    feat : new-item-container 퍼블리싱 작업

--- src/components/Item/NewItemContainer.js
+++ src/components/Item/NewItemContainer.js
@@ -1,10 +1,17 @@
 import React,{useState, useEffect} from 'react';
+import './NewItemContainer.css';
 
 const NewItemContainer = () => {
 
     return(
-        <div>
-
+        <div className="new-item-container">
+            <div className="border-block"></div>
+            <div className="block-title">
+                <p>새로운 거래</p>
+            </div>
+            <div className="new-item-form">
+                
+            </div>
         </div>
     );
 }
