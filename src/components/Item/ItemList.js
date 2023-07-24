b797cacdb69edc3cc035362b05d525ed1375e2e7
yuhyeon99 <jujini31@gmail.com>
2023-07-24 23:49:04 +0900

    feat : itemList map 사용해서 구현

--- src/components/Item/ItemList.js
+++ src/components/Item/ItemList.js
@@ -1,19 +1,27 @@
 import React,{useState, useEffect} from 'react';
 import './ItemList.css';
+import { useRecoilValue } from 'recoil';
+import { ItemAtom } from '../../recoil/ItemAtom';
 
 const ItemList = () => {
-
+    const items = useRecoilValue(ItemAtom);
     return(
         <div className="ItemList">
-            <div className="item">
-                <div className="item-info">
-                    <p>2023. 07. 22</p>
-                    <p>알바비</p>
-                </div>
-                <div className="item-money">
-                    <p> +800,000 </p>
-                </div>
-            </div>
+            {items.length === 0 ? (
+                <></>
+            ) : (
+                items.map((item, index) => (
+                    <div className="item">
+                        <div className="item-info">
+                            <p className="info-date">{item.year}. {item.month}. {item.day}</p>
+                            <p className="info-title">{item.description}</p>
+                        </div>
+                        <div className="item-money">
+                            <p className="money-plus"> +{parseInt(item.amount).toLocaleString()} </p>
+                        </div>
+                    </div>
+                ))
+            )}
         </div>
     );
 }
